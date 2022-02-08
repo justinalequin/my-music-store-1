c01e6641c7dcb54bc2787c2f999fcb7f0f2ead50
Omar Suriel <omarsuriel@icloud.com>
2022-02-08 15:53:48 -0500

    cart page shows products using mocked shopping cart

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,41 +1,57 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 import CartItem from '../CartItem';
 import Layout from '../layout/Layout';
+import Button from '@mui/material/Button';
+import ReplayIcon from '@mui/icons-material/Replay';
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from 'react-router-dom';
+
+const shoppingCart = [
+  {
+    id: '123',
+    title: 'Blue Drumset"',
+    price: 89999,
+    quantity: 2,
+    image: 'https://img.grouponcdn.com/stores/xeedMRUK7jjx6xqd8S8mD7aNKuL/storespi892307-1000x600/v1/c870x524.jpg',
+  },
+  {
+    id: '111',
+    title: 'Big piano',
+    price: 299999,
+    quantity: 2,
+    image: 'https://kawaius.com/wp-content/uploads/2020/07/Kawai-DG30-Digital-Grand.jpg',
+  },
+];
 
 const CartPage = () => {
   return (
     <Layout>
-      <h1>
-        This is the Shopping cart 
-      </h1>
       <Box p={4}>
-        <Box mb={4}>
-          <CartItem cartItem={{
-            id: '123',
-            title: 'Drum set blue',
-            image: 'https://media.sweetwater.com/images/items/750/SBP8F3CR-large.jpg?v=59a77bcd3841c67a',
-            quantity: 2,
-            price: 89999,
-          }} />
+        {shoppingCart.map(item => (
+          <Box mb={4} key={item.id}>
+            <CartItem cartItem={{
+              id: item.id,
+              title: item.title,
+              image: item.image,
+              quantity: item.quantity,
+              price: item.price,
+            }} />
+          </Box>
+        ))}
+        <Box display="flex" justifyContent="center" mb={2}>
+          <Typography>Total: $9999.99</Typography>
         </Box>
-        <Box mb={4}>
-          <CartItem cartItem={{
-            id: '123',
-            title: 'Drum set blue',
-            image: 'https://media.sweetwater.com/images/items/750/SBP8F3CR-large.jpg?v=59a77bcd3841c67a',
-            quantity: 2,
-            price: 89999,
-          }} />
+        <Box display="flex" justifyContent="center" mb={2}>
+          <Button sx={{width: '220px'}} variant="contained">Checkout</Button>
         </Box>
-        <Box mb={4}>
-          <CartItem cartItem={{
-            id: '123',
-            title: 'Drum set blue',
-            image: 'https://media.sweetwater.com/images/items/750/SBP8F3CR-large.jpg?v=59a77bcd3841c67a',
-            quantity: 2,
-            price: 89999,
-          }} />
+        <Box display="flex" justifyContent="center" mb={2}>
+          <Button sx={{width: '220px'}} startIcon={<ReplayIcon />} variant="contained">Empty cart</Button>
+        </Box>
+        <Box display="flex" justifyContent="center" mb={2}>
+          <Link to="/">
+            <Button sx={{width: '220px'}}  startIcon={<HomeIcon />} variant="contained">Back to home page</Button>
+          </Link>
         </Box>
       </Box>
     </Layout>
