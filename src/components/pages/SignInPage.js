d60382ccf5ff7c411bb9d87f5ce8774f75bcbded
JUSTIN ALEQUIN <justinalequin@JUSTINs-MacBook-Pro.local>
2022-02-22 14:10:57 -0500

    unable to finish before lunch

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { fetchUser } from '../../fetchData';
-import Layout from '../layout/Layout';
+import React from "react";
+import { fetchUser } from "../../fetchData";
+import Layout from "../layout/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { userSignedInAction } from "../../reduxStore";
 
-const SignInPage = () => {
+const SignInPage = (props) => {
+  const dispatch = useDispatch();
 
-  const handleUserLogIn = () => fetchUser('fake@user.com', '123')
-  //Put the data on redux
-  .then((data) => console.log('data: ', data));
+  const handleUserLogIn = () =>
+    fetchUser("fake@user.com", "123")
+      //Put the data on redux
+      .then((data) => console.log("data: ", data.firstName));
+
+  const userSingedIn = () => {
+    dispatch({
+      type: userSignedInAction,
+    });
+  };
+
+  const reduxState = useSelector((state) => state);
+
+  console.log("reduxState: ", reduxState);
 
   return (
     <Layout>
       <h1>Sign In page</h1>
       <button onClick={handleUserLogIn}>Log in user</button>
+      <button onClick={userSingedIn}>Click</button>
     </Layout>
-  )
+  );
 };
 
 export default SignInPage;
--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,42 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
 const shoppingCartInitialState = [];
 
-export const addItemToCartAction = 'addItemToCart';
+const userInitialState = false;
 
-export const removeItemFromCartAction = 'removeItemFromCart';
+export const addItemToCartAction = "addItemToCart";
 
-export const emptyCartAction = 'emptyCart';
+export const removeItemFromCartAction = "removeItemFromCart";
+
+export const emptyCartAction = "emptyCart";
+
+export const userSignedInAction = "userSignedIn";
+
+export const userNotSignedInAction = "userNotSignedIn";
 
 const shoppingCartReducer = (state = shoppingCartInitialState, action) => {
-
-  if(action.type === addItemToCartAction){
+  if (action.type === addItemToCartAction) {
     // write some logic to add item to cart
 
     // Check if the item is already in the cart
-    const itemFoundInCart = state.find(cartItem => cartItem.id === action.cartItem.id);
+    const itemFoundInCart = state.find(
+      (cartItem) => cartItem.id === action.cartItem.id
+    );
 
     // if its not, we can add it at the end with a quantity of 1
-    if(!itemFoundInCart){
-      return [...state, {...action.cartItem, quantity: 1}];
+    if (!itemFoundInCart) {
+      return [...state, { ...action.cartItem, quantity: 1 }];
     }
 
-    // if it is actually already in the cart    
+    // if it is actually already in the cart
     // we will still remove it
-    const cartWithFoundItemRemoved = state.filter(item => item.id !== action.cartItem.id);
+    const cartWithFoundItemRemoved = state.filter(
+      (item) => item.id !== action.cartItem.id
+    );
     // but add it again with the correct updated quantity.
-    return [...cartWithFoundItemRemoved, {...action.cartItem, quantity: itemFoundInCart.quantity + 1 }]
-    
+    return [
+      ...cartWithFoundItemRemoved,
+      { ...action.cartItem, quantity: itemFoundInCart.quantity + 1 },
+    ];
   }
 
-  if(action.type === removeItemFromCartAction){
+  if (action.type === removeItemFromCartAction) {
     // write some logic to remove item fromn cart
 
-    return state.filter(item => item.id !== action.itemId)
+    return state.filter((item) => item.id !== action.itemId);
   }
 
-
-  if(action.type === emptyCartAction){
+  if (action.type === emptyCartAction) {
     // write some logic to empty the shopping cart
     return shoppingCartInitialState;
   }
@@ -44,10 +54,26 @@ const shoppingCartReducer = (state = shoppingCartInitialState, action) => {
   return state;
 };
 
+// On sign in, I'm getting the user name returned from "data.firstName"
+// I need to set that as the state in redux so that I can access that in my header component to
+//change the signin button to user name being displayed.
+
+const userReducer = (state = userInitialState, action) => {
+  if (action.type === userSignedInAction) {
+    console.log("Signed In");
+    return state;
+  }
+  if (action.type === userNotSignedInAction) {
+    console.log("Not Signed In");
+    return state;
+  }
+};
+
 const store = configureStore({
   reducer: {
-    shoppingCart:  shoppingCartReducer,
+    shoppingCart: shoppingCartReducer,
+    user: userReducer,
   },
-})
+});
 
 export default store;
